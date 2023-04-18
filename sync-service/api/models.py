caae798bb9ade0f728a48de9732fd7fde42e47f4
samuel cho <samonthepiano@gmail.com>
2023-04-18 21:10:28 +0200

    expand schemas

--- sync-service/api/models.py
+++ sync-service/api/models.py
@@ -3,34 +3,34 @@ import datetime
 
 
 class Title(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     string = db.Column(db.String(200))
 
 
 class Subtitle(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     string = db.Column(db.String(300))
 
 
 class FullYear(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    string = db.Column(db.String(30))
+    id = db.Column(db.BigInteger, primary_key=True)
+    string = db.Column(db.String(48))
 
 
 class Composer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     string = db.Column(db.String(32))
     first_name = db.Column(db.String(32))
     last_name = db.Column(db.String(32))
 
 
 class Piece(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.Integer)
-    subtitle = db.Column(db.Integer)
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    title = db.Column(db.BigInteger)
+    subtitle = db.Column(db.BigInteger)
     year = db.Column(db.Integer)
-    full_year = db.Column(db.String(32))
-    composer = db.Column(db.Integer)
+    full_year = db.Column(db.BigInteger)
+    composer = db.Column(db.BigInteger)
     duration = db.Column(db.Integer)
 
 
