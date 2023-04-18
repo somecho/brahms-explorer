1e8bedc05b851b94a8f21298d0688755ae21b3e5
samuel cho <samonthepiano@gmail.com>
2023-04-18 21:30:17 +0200

    optimize schema and db session calls

--- sync-service/api/models.py
+++ sync-service/api/models.py
@@ -3,34 +3,34 @@ import datetime
 
 
 class Title(db.Model):
-    id = db.Column(db.BigInteger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     string = db.Column(db.String(200))
 
 
 class Subtitle(db.Model):
-    id = db.Column(db.BigInteger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     string = db.Column(db.String(300))
 
 
 class FullYear(db.Model):
-    id = db.Column(db.BigInteger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     string = db.Column(db.String(48))
 
 
 class Composer(db.Model):
-    id = db.Column(db.BigInteger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     string = db.Column(db.String(32))
     first_name = db.Column(db.String(32))
     last_name = db.Column(db.String(32))
 
 
 class Piece(db.Model):
-    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-    title = db.Column(db.BigInteger)
-    subtitle = db.Column(db.BigInteger)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Integer)
+    subtitle = db.Column(db.Integer)
     year = db.Column(db.Integer)
-    full_year = db.Column(db.BigInteger)
-    composer = db.Column(db.BigInteger)
+    full_year = db.Column(db.Integer)
+    composer = db.Column(db.Integer)
     duration = db.Column(db.Integer)
 
 
--- sync-service/api/sync.py
+++ sync-service/api/sync.py
@@ -5,26 +5,28 @@ from requests import Timeout
 from . import scraper
 from .models import Title, Subtitle, Composer, Log, Piece, FullYear
 from . import db
-from hashlib import md5
+# from hashlib import md5
 import click
 import time
 
 bp = Blueprint('api', __name__, url_prefix="/api")
 
 
-def hash_string(s: str) -> int:
-    return int(md5(s.encode("UTF-8")).hexdigest(), 16) % 10 ** 8
+# def hash_string(s: str) -> int:
+    # return int(md5(s.encode("UTF-8")).hexdigest(), 16) % 10 ** 8
 
+def hash_string(s: str)->int:
+    return hash(s) % 10 ** 8
 
 def piece_exists(piece: dict) -> bool:
     """checks if piece exists in database"""
     title_exist = (db.session
                    .query(Title.id)
-                   .filter_by(id=hash(piece["title"]))
+                   .filter_by(id=hash_string(piece["title"]))
                    .count())
     subtitle_exist = (db.session
                       .query(Subtitle.id)
-                      .filter_by(id=hash(piece["subtitle"]))
+                      .filter_by(id=hash_string(piece["subtitle"]))
                       .count())
     return True if title_exist and subtitle_exist else False
 
@@ -33,16 +35,16 @@ def composer_exists(composer: dict) -> bool:
     """checks if composer exists in database"""
     result = (db.session
               .query(Composer.id)
-              .filter_by(id=hash(composer["full_name"]))
+              .filter_by(id=hash_string(composer["full_name"]))
               .count())
     return True if result else False
 
 
 def add_composer(composer: dict):
     full_name = composer["full_name"]
-    print(f"Adding {full_name}")
+    # print(f"Adding {full_name}")
     db.session.add(Composer(
-        id=hash(full_name),
+        id=hash_string(full_name),
         string=full_name,
         first_name=composer["first_name"],
         last_name=composer["last_name"],
@@ -51,19 +53,19 @@ def add_composer(composer: dict):
 
 def add_piece(piece: dict):
     title = piece["title"]
-    hash_title = hash(title)
+    hash_title = hash_string(title)
     composer = piece["composer"]
     subtitle = piece["subtitle"]
-    hash_sub = hash(subtitle)
+    hash_sub = hash_string(subtitle)
     full_year = piece["full_year"]
-    hash_year = hash(full_year)
-    print(f"Adding {title} by {composer}")
+    hash_year = hash_string(full_year)
+    # print(f"Adding {title} by {composer}")
     db.session.add(Piece(
         title=hash_title,
         subtitle=hash_sub,
         year=piece["year"],
         full_year=hash_year,
-        composer=hash(composer),
+        composer=hash_string(composer),
         duration=piece["duration"]
     ))
     if not Title.query.filter_by(id=hash_title).count():
@@ -116,13 +118,13 @@ def update_database():
     print(f"Current number of pages is {num_pages}\n")
     start_time = time.time()
     num_pages = 5
-    offset_page = 372
+    offset_page = 5
     for i in range(offset_page, offset_page+num_pages):
         cur_page = i+1
         url = scraper.get_url(cur_page)
         print(f"Scraping page {cur_page} of {num_pages}")
         try:
-            with requests.get(url, timeout=10) as page:
+            with requests.get(url, timeout=3) as page:
                 print("Page request successful.")
                 print("Processing page...\n")
                 pieces = scraper.process_page(page)
