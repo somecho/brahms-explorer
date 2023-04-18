6b865d7c964285b3f7e78c816d83b9cf70e56c62
samuel cho <samonthepiano@gmail.com>
2023-04-18 09:46:28 +0200

    migrate models

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,11 +2,24 @@ from . import db
 
 
 class Piece(db.Model):
-    __table__ = db.Model.metadata.tables["pieces"]
+    __table__ = db.Model.metadata.tables["piece"]
 
 
 class Composer(db.Model):
-    __table__ = db.Model.metadata.tables["composers"]
+    __table__ = db.Model.metadata.tables["composer"]
+
 
 class Log(db.Model):
-    __table__ = db.Model.metadata.tables["logs"]
+    __table__ = db.Model.metadata.tables["log"]
+
+
+class Title(db.Model):
+    __table__ = db.Model.metadata.tables["title"]
+
+
+class Subtitle(db.Model):
+    __table__ = db.Model.metadata.tables["subtitle"]
+
+
+class FullYear(db.Model):
+    __table__ = db.Model.metadata.tables["full_year"]
