1432455af556074c1f7d8ebef56d670ef714487c
samuel cho <samonthepiano@gmail.com>
2023-04-24 13:38:22 +0200

    optimize query using match instead of like

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,6 +1,6 @@
 from .models import Piece, Composer, Log, Title, Subtitle, FullYear
 from flask import Blueprint, request
-from sqlalchemy import or_, and_
+from sqlalchemy import or_, and_, func
 from . import db
 
 
@@ -50,6 +50,12 @@ def process_ascending(str):
     return eval(str.capitalize())
 
 
+def process_count(str):
+    if not str:
+        return False
+    return eval(str.capitalize())
+
+
 map_order_by = {
     "title": Title.string,
     "lastName": Composer.last_name,
@@ -62,41 +68,44 @@ map_order_by = {
 @bp.route("/pieces", methods=["POST", "GET"])
 def pieces():
     if request.method == "GET":
-        limit = request.args.get("limit") or 100
+        limit = request.args.get("limit") or 10
         offset = request.args.get("offset") or 0
+        count = process_count(request.args.get("count"))
         keywords = process_keywords(request.args.get("keywords"))
         order_by = request.args.get("orderBy") or "title"
         ascending = process_ascending(request.args.get("ascending"))
-        or_queries = [or_(Title.string.contains(w),
-                          Subtitle.string.contains(w),
-                          Composer.string.contains(w),
-                          FullYear.string.contains(w)) for w in keywords]
+        or_queries = [or_(Title.string.match(w),
+                          Subtitle.string.match(w),
+                          Composer.string.match(w),
+                          FullYear.string.match(w)) for w in keywords]
         order = (map_order_by[order_by]
                  if ascending
                  else map_order_by[order_by].desc())
-        results = (db.session.query(Title.string,
-                                    Subtitle.string,
-                                    Composer.string,
-                                    FullYear.string,
-                                    Piece.duration,
-                                    Piece.id)
-                   .select_from(Piece)
-                   .join(Composer, Piece.composer == Composer.id)
-                   .join(Title, Piece.title == Title.id)
-                   .join(Subtitle, Piece.subtitle == Subtitle.id)
-                   .join(FullYear, Piece.full_year == FullYear.id)
-                   .filter(and_(*or_queries))
-                   .order_by(order)
-                   .limit(limit)
-                   .offset(offset)
-                   .all())
+        query = (db.session.query(Title.string,
+                                  Subtitle.string,
+                                  Composer.string,
+                                  FullYear.string,
+                                  Piece.duration,
+                                  Piece.id)
+                 .select_from(Piece)
+                 .join(Composer, Piece.composer == Composer.id)
+                 .join(Title, Piece.title == Title.id)
+                 .join(Subtitle, Piece.subtitle == Subtitle.id)
+                 .join(FullYear, Piece.full_year == FullYear.id)
+                 .filter(and_(*or_queries))
+                 .order_by(order)
+                 .limit(limit)
+                 .offset(offset))
         results = [dict(title=a[0],
                         subtitle=a[1],
                         composer=a[2],
                         year=a[3],
                         duration=a[4],
-                        id=a[5]) for a in results]
-        return results
+                        id=a[5]) for a in query.all()]
+        if count:
+            total_count = query.with_entities(func.count(Piece.id)).scalar()
+            return {"results": results, "count": total_count}
+        return {"results": results}
 
 
 @bp.route("/last-updated")
