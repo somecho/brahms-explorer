0ae05b628165150192c986c6faa0d037d5f12dde
Somē Cho <itssomicho@gmail.com>
2023-05-01 11:48:03 +0200

    add comment in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.05.tar.gz") { } }:
 pkgs.mkShell {
   packages = [
+	  # Render uses python 3.7.10
     (pkgs.python37.withPackages (ps: with ps;[
       pip
       virtualenv
